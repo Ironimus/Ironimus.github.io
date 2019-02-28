541ef631b75f9c31790e63288feb7d4b9029fa66
Yevstakhiy Krul <ironimus13@gmail.com>
2019-02-28 06:42:39 -0800

    gh-pages fix

---
+++ jsconfig.js
@@ -0,0 +1,16 @@
+{
+  "compilerOptions": {
+    "target": "ES6",
+    "allowSyntheticDefaultImports": true,
+    "baseUrl": "./src/",
+    "paths": {
+      "*": [
+        "*"
+      ]
+    }
+  },
+  "exclude": [
+    "node_modules",
+    "**/node_modules/*"
+  ]
+}
